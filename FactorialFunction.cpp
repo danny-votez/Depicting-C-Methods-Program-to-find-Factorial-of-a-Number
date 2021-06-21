de93ac7869d65e68f1282bf8deb139d6a00cbe0c
Danny-Votez <77758884+danny-votez@users.noreply.github.com>
2021-06-21 21:36:56 +0300

    Add files via upload

---
+++ FactorialFunction.cpp
@@ -0,0 +1,27 @@
+// This program is aimed at depicting C++ Methods
+// To do this, there is using an example of a program that calculates factorial of a number
+// That is, with factorial, the operation is: n! = n*(n-1)*(n-2)*(n-3)........4*3*2*1
+
+#include<iostream>
+using namespace std;
+class Method{
+    private: int Number;
+    public: void Get(){
+        cout<<"Enter a number you wish to find its Factorial: "<<endl;
+        cin>>Number;
+        cout<<"The result of "<<Number<<"! is: "<<Factor(Number);   /// Initiate a call
+    }
+    public: int Factor(int N){
+        if (N == 0){                     // This forms the base condition
+            return 1;
+        }
+        else{
+            return N*Factor(N-1);       // This is for example, 5!  = 5*4*3*3*1
+        }
+    }
+};
+int main(){
+    Method M;
+    M.Get();
+    return 0;
+}
